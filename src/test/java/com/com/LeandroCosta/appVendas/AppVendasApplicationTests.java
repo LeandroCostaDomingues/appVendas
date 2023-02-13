4d523f6fd43043a81940535164b3f748cb358abf
Leandro Costa <leandrocosta190991@gmail.com>
2023-02-13 15:34:07 -0300

    Projeto criado com base de dados criado automaticamente

---
+++ src/test/java/com/com/LeandroCosta/appVendas/AppVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.com.LeandroCosta.appVendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppVendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
