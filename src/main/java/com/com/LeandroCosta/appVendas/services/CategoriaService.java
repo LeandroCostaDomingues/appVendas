818ca34686c0eb82cc499c32d55a60f2b0ee93be
Leandro Costa <leandrocosta190991@gmail.com>
2023-02-13 20:12:41 -0300

    ajustes  no endpoint categoria/{id} com tratamento de exceções

--- src/main/java/com/com/LeandroCosta/appVendas/services/CategoriaService.java
+++ src/main/java/com/com/LeandroCosta/appVendas/services/CategoriaService.java
@@ -7,15 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.com.LeandroCosta.appVendas.domain.Categoria;
 import com.com.LeandroCosta.appVendas.repositories.CategoriaRepository;
+import com.com.LeandroCosta.appVendas.services.exceptions.ObjectNotFoundException;
 
-@Service   // classe de serviço 
+@Service // classe de serviço
 public class CategoriaService {
 	@Autowired // dependecia
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
---
+++ src/main/java/com/com/LeandroCosta/appVendas/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.com.LeandroCosta.appVendas.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+
+		super(msg, cause);
+	}
+}
