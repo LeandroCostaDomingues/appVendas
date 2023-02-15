562f6b4f500e9eaa9d51ec7c154295fb49370d18
Leandro Costa <leandrocosta190991@gmail.com>
2023-02-15 15:57:45 -0300

    buscando todas categorias e criando operaçao com DTO e convertendo uma lista categroia para  ListaDTO

--- src/main/java/com/com/LeandroCosta/appVendas/resources/CategoriaResource.java
+++ src/main/java/com/com/LeandroCosta/appVendas/resources/CategoriaResource.java
@@ -1,6 +1,9 @@
 package com.com.LeandroCosta.appVendas.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.com.LeandroCosta.appVendas.domain.Categoria;
+import com.com.LeandroCosta.appVendas.dto.CategoriaDTO;
 import com.com.LeandroCosta.appVendas.services.CategoriaService;
 
 @RestController
@@ -42,6 +46,13 @@ public ResponseEntity<Void>update(@RequestBody Categoria obj,@PathVariable Integ
 public ResponseEntity<Void> delete(@PathVariable Integer id) {
 	service.delete(id);
 	return ResponseEntity.noContent().build();
+
+	}
+@RequestMapping(method = RequestMethod.GET) // bsuacr todas categorias  atravez da lista dto 
+public ResponseEntity<List<CategoriaDTO>> findAll() {
+List<Categoria> list = service.findAll();
+List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList()); // para converter uma lista  para outra 
+	return ResponseEntity.ok().body(listDTO);
 
 }
 }
