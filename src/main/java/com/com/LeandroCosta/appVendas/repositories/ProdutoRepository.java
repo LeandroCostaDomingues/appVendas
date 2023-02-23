d56fdc197899e5261493fe9ca3d6d37de6ecc6eb
Leandro Costa <leandrocosta190991@gmail.com>
2023-02-23 20:45:34 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/com/LeandroCosta/appVendas/repositories/ProdutoRepository.java
+++ src/main/java/com/com/LeandroCosta/appVendas/repositories/ProdutoRepository.java
@@ -1,15 +1,22 @@
 package com.com.LeandroCosta.appVendas.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-
+import com.com.LeandroCosta.appVendas.domain.Categoria;
 import com.com.LeandroCosta.appVendas.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
-	
-	// classe de acesso a dados 
-	
-
+	@Transactional(readOnly=true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
